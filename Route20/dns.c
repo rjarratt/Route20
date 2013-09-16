dbf3868b049b1cf96be780d6308b6ca42c47b0e9
Rob Jarratt <rjarratt@users.noreply.github.com>
2013-09-16 07:12:33 +0000

    Don't send DNS queries for IP addresses. Improved logging of DNS

--- Route20/dns.c
+++ Route20/dns.c
@@ -66,11 +66,11 @@ static socket_t DnsSocket;
 static sockaddr_t DnsServer;
 static callback_entry_t *callbackList = NULL;
 
-
+static int IsValidNameToQuery(char *name);
 static void DnsProcessResponse(void *context);
 static callback_entry_t *FindCallbackEntry(int id);
 static callback_entry_t *CreateCallbackEntry(int id);
-static int ParseQuestion(packet_t *packet, int *currentOffset);
+static int ParseQuestion(packet_t *packet, int *currentOffset, char **name);
 static int ParseResource(packet_t *packet, int *currentOffset, uint16 *type, byte **data, int *dataLength);
 
 int DnsOpen(char *serverName)
@@ -96,58 +96,82 @@ void DnsSendQuery(char *name, uint16 id, void (*callback)(byte *, void *), void 
 	byte i;
 	callback_entry_t *callbackEntry;
 
-	query.header.id = id;
-	query.header.flags = Uint16ToBigEndian(0x0100);
-	query.header.qdcount = Uint16ToBigEndian(0x0001);
-	query.header.ancount = 0;
-	query.header.nscount = 0;
-	query.header.arcount = 0;
+    if (IsValidNameToQuery(name))
+    {
+        query.header.id = id;
+        query.header.flags = Uint16ToBigEndian(0x0100);
+        query.header.qdcount = Uint16ToBigEndian(0x0001);
+        query.header.ancount = 0;
+        query.header.nscount = 0;
+        query.header.arcount = 0;
 
-	strncpy((char *)(query.data + 1), name, sizeof(query.data) - 5);
+        strncpy((char *)(query.data + 1), name, sizeof(query.data) - 5);
 
-	namePtr = name;
-	dataPtr = query.data;
-	i = 0;
+        namePtr = name;
+        dataPtr = query.data;
+        i = 0;
 
-	while (*namePtr != '\0')
-	{
-		if (*namePtr != '.')
-		{
-			i++;
-		}
-		else
-		{
-			*dataPtr = i;
-			dataPtr += i + 1;
-			i = 0;
-		}
+        while (*namePtr != '\0')
+        {
+            if (*namePtr != '.')
+            {
+                i++;
+            }
+            else
+            {
+                *dataPtr = i;
+                dataPtr += i + 1;
+                i = 0;
+            }
 
-		namePtr++;
-	}
+            namePtr++;
+        }
 
-	*dataPtr = i;
-	dataPtr += i + 2;
+        *dataPtr = i;
+        dataPtr += i + 2;
 
 
-	*dataPtr++ = 0;
-	*dataPtr++ = 1;
-	*dataPtr++ = 0;
-	*dataPtr++ = 1;
+        *dataPtr++ = 0;
+        *dataPtr++ = 1;
+        *dataPtr++ = 0;
+        *dataPtr++ = 1;
 
-	packet.rawData = (byte *)&query;
-	packet.rawLen = dataPtr - (byte *)&query;
+        packet.rawData = (byte *)&query;
+        packet.rawLen = dataPtr - (byte *)&query;
 
-	callbackEntry = FindCallbackEntry(id);
-	if (callbackEntry == NULL)
-	{
-		callbackEntry = CreateCallbackEntry(id);
-	}
+        callbackEntry = FindCallbackEntry(id);
+        if (callbackEntry == NULL)
+        {
+            callbackEntry = CreateCallbackEntry(id);
+        }
 
-	callbackEntry->callback = callback;
-	callbackEntry->callbackContext = callbackContext;
+        callbackEntry->callback = callback;
+        callbackEntry->callbackContext = callbackContext;
 
-	/*Log(LogInfo, "Sending DNS query for %s\n", name);*/
-	SendToSocket(&DnsSocket, &DnsServer, &packet);
+        Log(LogDns, LogDetail, "Sending DNS query for %s\n", name);
+        SendToSocket(&DnsSocket, &DnsServer, &packet);
+    }
+    else
+    {
+	    Log(LogDns, LogDetail, "Not sending DNS query for %s as it is already an IP address\n", name);
+    }
+}
+
+static int IsValidNameToQuery(char *name)
+{
+    int ans = 0;
+    int i;
+
+    for (i = 0; i < strlen(name); i++)
+    {
+        if (!isdigit(name[i]) && name[i] != '.')
+        {
+            ans = 1;
+            break;
+        }
+    }
+
+    return ans;
 }
 
 static void DnsProcessResponse(void *context)
@@ -173,10 +197,12 @@ static void DnsProcessResponse(void *context)
 			requestId = header->id;
 		    header->qdcount = BigEndianToUint16(header->qdcount);
 		    header->ancount = BigEndianToUint16(header->ancount);
-		    /*Log(LogInfo, "DNS response, questions: %d, answers: %d\n", header->qdcount, header->ancount);*/
+		    Log(LogDns, LogVerbose, "DNS response, questions: %d, answers: %d\n", header->qdcount, header->ancount);
 			for (i = 0; i < header->qdcount && ok; i++)
 			{
-				ok = ParseQuestion(&packet, &currentOffset);
+                char *name;
+				ok = ParseQuestion(&packet, &currentOffset, &name);
+                Log(LogDns, LogDetail, "Processing DNS response for %s", name);
 			}
 
 			for (i = 0; i < header->ancount && ok; i++)
@@ -188,10 +214,13 @@ static void DnsProcessResponse(void *context)
 					haveIp = 1;
 					if (callbackEntry != NULL)
 					{
+                        Log(LogDns, LogDetail, " address is %d.%d.%d.%d", data[0], data[1], data[2], data[3]);
 						callbackEntry->callback(data, callbackEntry->callbackContext);
 					}
 				}
 			}
+
+            Log(LogDns, LogDetail, "\n");
 		}
 
 		if (!ok)
@@ -236,9 +265,13 @@ static callback_entry_t *CreateCallbackEntry(int id)
 }
 
 
-static int ParseQuestion(packet_t *packet, int *currentOffset)
+static int ParseQuestion(packet_t *packet, int *currentOffset, char **name)
 {
+    static char parsedName[1024];
 	int ans = 0;
+
+    parsedName[0] = 0;
+    *name = parsedName;
 	if (packet->rawLen >= *currentOffset + 1)
 	{
 		int ok = 1;
@@ -248,6 +281,16 @@ static int ParseQuestion(packet_t *packet, int *currentOffset)
 		{
 			segLength = (((int)packet->rawData[*currentOffset]) & 0xFF) + 1;
 			totalLength += segLength;
+
+            if (strlen(parsedName) > 0)
+            {
+                strcat(parsedName, ".");
+            }
+            if ((strlen(parsedName) + segLength - 2) < sizeof(parsedName))
+            {
+                    strncat(parsedName, (char *)&packet->rawData[*currentOffset + 1], segLength - 1);
+            }
+
 			*currentOffset += segLength;
 		    ok = packet->rawLen >= totalLength;
 		} while (ok && segLength > 1);
